b6c86a662aa3937e9da7f4d9f18b5cc25f340fbe
k0000k <locker50@naver.com>
2022-11-09 17:13:08 +0900

    새 글 크롤링 기능 추가

---
+++ src/main/java/com/veloginmobile/server/crawler/NewPostDto.java
@@ -0,0 +1,12 @@
+package com.veloginmobile.server.crawler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewPostDto {
+    List<String> newPostAuthor;
+
+    public NewPostDto() {
+        newPostAuthor = new ArrayList<>();
+    }
+}
--- src/main/java/com/veloginmobile/server/data/repository/SubscribeRepository.java
+++ src/main/java/com/veloginmobile/server/data/repository/SubscribeRepository.java
@@ -4,7 +4,11 @@ import com.veloginmobile.server.data.entity.Subscribe;
 import com.veloginmobile.server.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SubscribeRepository  extends JpaRepository<Subscribe, Long> {
 
     Subscribe findByUser(User user);
+    //List<String> findAllSubscribers();
+
 }
