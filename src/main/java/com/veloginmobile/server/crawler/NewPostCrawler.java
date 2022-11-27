308a3b870edd0ca28b72ff051caa9e3a8320edff
k0000k <locker50@naver.com>
2022-11-27 23:29:55 +0900

    크롤러 주기 재설정

--- src/main/java/com/veloginmobile/server/crawler/NewPostCrawler.java
+++ src/main/java/com/veloginmobile/server/crawler/NewPostCrawler.java
@@ -6,45 +6,40 @@ import com.veloginmobile.server.data.repository.CrawlingRepository;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.PersistenceUnit;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class NewPostCrawler {
 
     CrawlingRepository crawlingRepository;
 
+    @Autowired
+    public NewPostCrawler (CrawlingRepository crawlingRepository) {
+        this.crawlingRepository = crawlingRepository;
+    }
+
     public void searchNewPost() throws IOException {
-        while (Boolean.TRUE){
-            List<Crawling> allSubscribers = crawlingRepository.findAll();
+        List<Crawling> allSubscribers = crawlingRepository.findAll();
 
-            for(Crawling subscriber : allSubscribers) {
+        for(Crawling subscriber : allSubscribers) {
 
-                String velogUser = subscriber.getVelogUserName();
-                NewPostDto newPostDto = new NewPostDto();
+            String velogUser = subscriber.getVelogUserName();
+            NewPostDto newPostDto = new NewPostDto();
 
-                String userProfileURL = "https://velog.io/@" + velogUser;
-                Document doc = Jsoup.connect(userProfileURL).get();
+            String userProfileURL = "https://velog.io/@" + velogUser;
+            Document doc = Jsoup.connect(userProfileURL).get();
 
-                Elements posts = doc.select(".subinfo span");
-                String writeTime = posts.first().text();
-                System.out.println(writeTime);
+            Elements posts = doc.select(".subinfo span");
+            String writeTime = posts.first().text();
 
-                if (writeTime.equals("1분 전") || writeTime.equals("2분전")) {
-                    newPostDto.newPostAuthor.add(velogUser);
-                    System.out.println(newPostDto);
-                }
-            }
-            try {
-                Thread.sleep(30000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            if (writeTime.equals("1분 전") || writeTime.equals("2분전") || writeTime.equals("3분전")|| writeTime.equals("4분전")|| writeTime.equals("5분전")) {
+                newPostDto.newPostAuthor.add(velogUser);
             }
         }
+        // 알림 함수 호출 부분
     }
 }
